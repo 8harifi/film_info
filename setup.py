98993dca74892beb0362cfbd60b5622c18c51dec
m sharifi <72849089+8harifi@users.noreply.github.com>
2021-08-12 18:13:02 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
     version='1.1.1',
     description='finds any film on imdb.com and returns some info about it',
     long_description=open(os.path.join(DIR, 'README.md')).read(),
-    author='the-liz4rd',
-    author_email='oliver.the.lizard@yandex.com',
-    url='https://github.com/the-liz4rd/film_info',
+    author='8harifi',
+    author_email='8harifi@gmail.com',
+    url='https://github.com/8harifi/film_info',
     py_modules=['film_info'],
     entry_points={
         'console_scripts':
